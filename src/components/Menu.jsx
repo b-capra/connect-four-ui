9e6fc78f9588425fe8595e87c65761f6c6730b86
b-capra <caprabmc@gmail.com>
2023-01-30 19:04:38 -0500

    Add menu logo; adjust spacing; add tablet styles

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,6 +3,7 @@ import './Menu.css'
 const Menu = () => {
   return (
     <div className="Menu">
+      <img className='menuLogo' src='/src/assets/images/logo.svg' />
       <button className='menuButton' id='cpuBtn'>PLAY VS CPU</button>
       <button className='menuButton' id='playerBtn'>PLAY VS PLAYER</button>
       <button className='menuButton' id='rulesBtn'>GAME RULES</button>
