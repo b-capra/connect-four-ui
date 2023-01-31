4868e3e9055914cc549d308261a0f400daab88e8
b-capra <caprabmc@gmail.com>
2023-01-31 12:24:11 -0500

    Add images to menu buttons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,8 +4,14 @@ const Menu = () => {
   return (
     <div className="Menu">
       <img className='menuLogo' src='/src/assets/images/logo.svg' />
-      <button className='menuButton' id='cpuBtn'>PLAY VS CPU</button>
-      <button className='menuButton' id='playerBtn'>PLAY VS PLAYER</button>
+      <button className='menuButton' id='cpuBtn'>
+        PLAY VS CPU
+        <img className='btnImg' src='/src/assets/images/player-vs-cpu.svg' />
+      </button>
+      <button className='menuButton' id='playerBtn'>
+        PLAY VS PLAYER
+        <img className='btnImg' src='/src/assets/images/player-vs-player.svg' />
+      </button>
       <button className='menuButton' id='rulesBtn'>GAME RULES</button>
     </div>
   )
